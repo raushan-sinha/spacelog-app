f67564da6750937783cab9f4906a8c165c543059
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-28 23:23:40 +0530

    Removed previous code & Added new component - Navbar

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,7 @@
-import { Text, View } from "react-native";
+import { Navbar } from "@/components/layout";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{color: 'red', fontSize: 18, fontFamily: 'monospace'}}>Welcome to my First Mobile App - Spacelog</Text>
-      <Text style={{color: 'red', fontSize: 18, fontFamily: 'monospace'}}>Hello</Text>
-      <Text style={{color: 'green', fontSize: 18, fontFamily: 'monospace'}}>Hello</Text>
-      <Text style={{color: 'green', fontSize: 18, fontFamily: 'monospace'}}>Hello</Text>
-      <Text style={{color: 'green', fontSize: 18, fontFamily: 'monospace'}}>Hello</Text>
-    </View>
-  );
+    return (
+        <Navbar />
+    )
 }
