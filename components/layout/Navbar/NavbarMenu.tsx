4d3fe4d6849e3185807be80c59587e75f75875ca
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-01 23:22:10 +0530

    Added logic for Menu Items

---
+++ components/layout/Navbar/NavbarMenu.tsx
@@ -0,0 +1,47 @@
+import { FlatList, Pressable, Text, View } from "react-native";
+import { MenuItemProps } from "./types";
+import { menuItems } from "./data";
+
+interface MenuCloseProps {
+    onClose: () => void
+}
+
+export default function NavbarMenu({ onClose }: MenuCloseProps) {
+    const renderItem = ({ item }: { item: MenuItemProps }) => {
+        return (
+            <Pressable
+                style={({ pressed }) => ({
+                    paddingVertical: 14,
+                    paddingHorizontal: 16,
+                    borderBottomWidth: 1,
+                    borderBottomColor: '#222',
+                    backgroundColor: pressed ? '#1a1a1a' : 'transparent',
+                })}
+                onPress={onClose}
+            >
+                <Text style={{ color: 'yellow', fontFamily: 'monospace', fontSize: 16 }}>
+                    {item.label}
+                </Text>
+            </Pressable>
+        )
+    }
+
+    return (
+        <View
+            style={{
+                marginHorizontal: 16,
+                backgroundColor: '#111',
+                borderBottomLeftRadius: 12,
+                borderBottomRightRadius: 12,
+                elevation: 5,
+                overflow: 'hidden',
+                top: 5
+            }}>
+            <FlatList
+                data={menuItems}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+            />
+        </View>
+    );
+}
