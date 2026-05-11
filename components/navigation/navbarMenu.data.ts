d2a4adbde9e92d052678376907bd4ccecae64f1d
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-11 13:30:08 +0530

    Added Menu links data

---
+++ components/navigation/navbarMenu.data.ts
@@ -0,0 +1,8 @@
+import { NavbarMenuTypes } from "./navbar.types";
+
+export const NAVMENU_LINKS: NavbarMenuTypes[] = [
+    { id: 1, label: 'Home' },
+    { id: 2, label: 'About' },
+    { id: 3, label: 'Docs' },
+    { id: 4, label: 'Contact' },
+];
