e3dd9df2be6f12e4a459d882da086e41a112db21
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-12 13:22:00 +0530

    Created a file for App

---
+++ app/contact.tsx
@@ -0,0 +1,10 @@
+
+import { Text, View } from "react-native";
+
+export default function Contact() {
+    return (
+        <View>
+            <Text>Contact</Text>
+        </View>
+    );
+}
---
+++ app/docs.tsx
@@ -0,0 +1,10 @@
+
+import { Text, View } from "react-native";
+
+export default function Docs() {
+    return (
+        <View>
+            <Text>Docs</Text>
+        </View>
+    );
+}
