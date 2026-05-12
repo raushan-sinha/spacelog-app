ac9d9bdb1fe661ff62b868361910ff82f1c31523
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-12 16:52:42 +0530

    Created a  file and added all sections files in clean form

---
+++ components/about/index.ts
@@ -0,0 +1,4 @@
+export { default as AboutHero } from './AboutHero';
+export { default as MissionSection } from './MissionSection';
+export { default as FeatureSection } from './FeatureSection';
+export { default as TechStack } from './TechStack';
