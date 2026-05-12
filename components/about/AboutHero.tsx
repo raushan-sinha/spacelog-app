c50b49310a0fa89d37dc4fe217fa9d6b55924c7b
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-12 16:12:48 +0530

    Created a folder and added sections for About page

---
+++ components/about/AboutHero.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from "react-native"
+
+export default function AboutHero() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                About Spacelog
+            </Text>
+
+            <Text style={styles.subtitle}>
+                Explore the universe through daily space discoveries,
+                astronomy insights, rocket launches, and stunning cosmic imagery.
+            </Text>
+        </View>
+    )
+}
+
+
+//todo: Styles -
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        marginTop: 20,
+        backgroundColor: '#168e6a',
+        borderRadius: 14,
+        margin: 20,
+    },
+
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#ffffff',
+        marginBottom: 10,
+    },
+
+    subtitle: {
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#cfcfcf',
+    },
+})
---
+++ components/about/MissionSection.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function MissionSection() {
+    return (
+        <View style={styles.container}>
+
+            <Text style={styles.heading}>
+                Our Mission
+            </Text>
+
+            <Text style={styles.description}>
+                Spacelog is designed to make space exploration
+                simple, modern, and accessible for everyone.
+
+                Our goal is to help users stay connected with
+                the latest discoveries from space agencies,
+                astronomy research, and cosmic events happening
+                across the universe.
+            </Text>
+
+        </View>
+    )
+}
+
+
+//todo: Styles - 
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        backgroundColor: '#101923',
+        borderRadius: 14,
+        margin: 20,
+    },
+
+    heading: {
+        fontSize: 24,
+        fontWeight: '700',
+        color: '#fbab57',
+        marginBottom: 12,
+    },
+
+    description: {
+        fontSize: 16,
+        lineHeight: 26,
+        color: '#f5f5f5',
+    },
+})
