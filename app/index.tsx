85f1c359c15d287db380d7aa07608d02410978dd
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-12 13:24:30 +0530

    code updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { Navbar } from "@/components/navigation";
-import { View } from "react-native";
+
+import { Text, View } from "react-native";
 
 export default function Index() {
     return (
         <View>
-            <Navbar />
+            <Text>Home</Text>
         </View>
     );
 }
