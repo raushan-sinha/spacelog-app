812e297f8ca702201680ab4367c78af225722653
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-13 11:25:44 +0530

    Added new styles with Scroll logic and added new component

--- app/docs.tsx
+++ app/docs.tsx
@@ -1,10 +1,24 @@
 
-import { Text, View } from "react-native";
+import { DocsHero } from "@/components/docs";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Docs() {
     return (
-        <View>
-            <Text>Docs</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                <DocsHero />
+            </View>
+        </ScrollView>
     );
 }
+
+
+//todo: Styles - 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#e4e4e4',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        gap: 5,
+    },
+})
