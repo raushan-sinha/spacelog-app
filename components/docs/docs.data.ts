c0eb4c7184ba6aa3a469327f27c65c4d5b1d12db
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-14 23:05:12 +0530

    Added data for Features

---
+++ components/docs/docs.data.ts
@@ -0,0 +1,23 @@
+import { FeatureGuideProps } from "./docs.types";
+
+export const FEATURES_GUIDE: FeatureGuideProps[] = [
+    {
+        title: 'Daily News Feed',
+        description: 'Browse the latest astronomy and space discoveries every day.',
+    },
+
+    {
+        title: 'HD Space Images',
+        description: 'View high quality cosmic imagery powered by NASA APIs.',
+    },
+
+    {
+        title: 'Rocket Launch Updates',
+        description: 'Track upcoming launches and mission details.',
+    },
+
+    {
+        title: 'Search Features',
+        description: 'Search planets, galaxies, and space missions easily.',
+    },
+]
