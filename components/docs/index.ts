c35395930bcd26e1a83444e6d45e228e4d074e9f
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-14 23:17:46 +0530

    Adde a new code file

--- components/docs/index.ts
+++ components/docs/index.ts
@@ -1,3 +1,4 @@
 export { default as DocsHero } from './DocsHero';
 export { default as GettingStarted } from './GettingStarted';
 export { default as FeaturesGuide } from './FeaturesGuide';
+export { default as FAQSection } from './FAQSection';
